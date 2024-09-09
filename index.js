81c1c02eb10d2c2ea3c8fb3eec54e18215678e90
Rafael Ferraz <140864570+RafaeldeAFerraz@users.noreply.github.com>
2024-09-09 23:39:00 +0000

    Endpoint Read By ID `[GET] / personagem/:id` criado

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+res.send('Hello World!')
 })
 
 const lista = ['Java', 'Kotlin', 'Android']
+//              0       1         2
 
 // Endpoint Read All [Get] /personagens
 app.get('/personagem', function (req, res) {
     res.send(lista)
+})
+
+// Endpoint Read by ID [GET] /personagem/:id
+app.get('/personagem/:id', function (req, res) {
+  // Acessamos o parâmetro de rota ID
+  const id = req.params.id
+
+  // Acessa o item na lista usando o ID - 1
+  const item = lista[id - 1]
+
+  // Enviamos o item como resposta
+  res.send(item)
 })
 
 app.listen(3000)
